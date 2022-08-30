fcef9efff8046011e0b100535bdd2afccca2dddd
Ryan Short <ryanshort.webdev@gmail.com>
2022-08-30 15:49:32 -0400

    functional update

--- main/main.js
+++ main/main.js
@@ -2,9 +2,9 @@ const toss = _ => {
   const coin = document.getElementById('coin');
   const flip = Math.random();
   coin.style.animation = 'none';
-  if(flip >= 0.5){
-    coin.style.animation = 'flipHeads 3s forwards';
+  if(flip > 0.5){
+    coin.style.animation = 'flipHeads 4s forwards';
   }else{
-    coin.style.animation = 'flipTails 3s forwards';
+    coin.style.animation = 'flipTails 4s forwards';
   }
 }
